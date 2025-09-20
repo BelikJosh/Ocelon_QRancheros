8d8cdc3b0b445649b3b04b9079faafab5328742e
DavidRangel2405 <21151065@aguascalientes.tecnm.mx>
2025-09-20 11:20:57 -0600

    Upgrade AppTabs

--- src/pages/AppTabs.tsx
+++ src/pages/AppTabs.tsx
@@ -125,7 +125,7 @@ const styles = StyleSheet.create({
   alignItems: 'center',
   justifyContent: 'center',
   },
-
+ 
   tabBarBg: {
     ...StyleSheet.absoluteFillObject,
     backgroundColor: '#121218',
